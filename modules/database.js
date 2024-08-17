dd20f6542d98fc762a6da0a4bceb59284b2d887e
eniyavan <eniyavan.b2003@gmail.com>
2024-08-17 16:33:18 +0530

    fourth commit

--- modules/database.js
+++ modules/database.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   ssl: {
+//     rejectUnauthorized: false, // Required for Render's self-signed SSL certificate
+//   },
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // For self-signed certificates
+  },
 });
 
-// pool.on('connect', () => {
-//   console.log('Connected to the PostgreSQL database');
-// });
+pool.on('connect', () => {
+  console.log('Connected to the PostgreSQL database');
+});
+
 
 module.exports = pool;
